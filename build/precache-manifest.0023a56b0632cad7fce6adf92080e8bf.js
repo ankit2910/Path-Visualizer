b86ae24d1d0f043eb6fe042b23c7b70b0f682bf5
Ankit Kumar <ankitkumar.ak05122002@gmail.com>
2023-07-29 16:21:57 +0530

    first commit

---
+++ build/precache-manifest.0023a56b0632cad7fce6adf92080e8bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3e99795193e62dcadc974e754061fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "111ec847d841df79dba6",
+    "url": "/static/css/main.7ed39f08.chunk.css"
+  },
+  {
+    "revision": "be9187377607dbc36b34",
+    "url": "/static/js/2.03edf62f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.03edf62f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111ec847d841df79dba6",
+    "url": "/static/js/main.dc4b9b7e.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "364b04b62e5b541e08866b292ea3803b",
+    "url": "/static/media/start.364b04b6.png"
+  }
+]);
